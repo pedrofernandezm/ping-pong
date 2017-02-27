ab8e4f66a1d0d78d67b120b06194eb2111775264
Pedro Fernandez <pedrofernandezm@gmail.com>
2017-02-27 18:52:43 -0500

    Add postgres configuration

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170227214316) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.date     "date_played"
     t.integer  "opponent_id"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170227214316) do
     t.integer  "rating",                 default: 1
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
